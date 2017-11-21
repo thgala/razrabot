701718f6819b91f2b62f45587622b0278bfa05a3
Konstantin <thgala@gmail.com>
2017-11-21 18:35:47 +0100

    Styles improvment;

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,10 @@ class Header extends Component {
     return (
       <div className='Header'>
         <LayoutContainer>
-          Hey hey!
+          <h1>Korem ipsum</h1>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </LayoutContainer>
       </div>
     )
